f6a7e960716311933b78dfcf5428bc032f929afd
unknown <shivamshah1210@gmail.com>
2023-06-25 20:18:01 +0530

    thode changes

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 
-
-
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+var hamburger = document.querySelector("#content h2");
+var nav = document.querySelector("#nav-bar");
+var flag = 0;
+hamburger.addEventListener("click", function(){
+    if(flag ===0){
+        nav.style.top = 0;
+       flag=1;
+    }else{
+        nav.style.top = "-100%";
+       flag=0;
+    }
+})
