9b744002a110404a454605652a77e89896edca36
unknown <shivamshah1210@gmail.com>
2023-06-26 14:20:44 +0530

    alomsit complest

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 
 var hamburger = document.querySelector("#content h2");
 var nav = document.querySelector("#nav-bar");
+var h2 = document.querySelector("#content h2")
 var flag = 0;
 hamburger.addEventListener("click", function(){
     if(flag ===0){
         nav.style.top = 0;
        flag=1;
+       h2.innerHTML = ``
     }else{
         nav.style.top = "-100%";
        flag=0;
