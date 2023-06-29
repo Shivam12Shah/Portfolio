7a1f9a9ae453d84a279fef216b2bc02272d07a23
unknown <shivamshah1210@gmail.com>
2023-06-29 13:41:21 +0530

    portfolio almost done

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ var hamburger = document.querySelector("#content h2");
 var nav = document.querySelector("#nav-bar");
 var h2 = document.querySelector("#content h2")
 var flag = 0;
+var main = document.querySelector("#main")
+var mouse = document.querySelector("#cursor")
+var title = document.querySelector("#intro h3")
+
+
+
 hamburger.addEventListener("click", function(){
     if(flag ===0){
         nav.style.top = 0;
@@ -12,4 +18,22 @@ hamburger.addEventListener("click", function(){
         nav.style.top = "-100%";
        flag=0;
     }
+})
+
+
+main.addEventListener("mousemove", function(dts){
+
+    mouse.style.left = `${dts.x}px`;
+    mouse.style.top = `${dts.y}px`;
+})
+
+title.addEventListener("mouseenter", function(){
+    mouse.style.scale = 10;
+    mouse.style.mixBlendMode= "color";
+
+})
+title.addEventListener("mouseleave", function(){
+    mouse.style.scale = 1;
+    mouse.style.mixBlendMode= "normal";
+
 })
